0a3a3524f87b7622335188a1a916671c26c9cd8e
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-09-29 10:14:17 +0530

    Create T1(b)_namespace.cpp

---
+++ practical 4/T1(b)_namespace.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+namespace first{
+  void sum(int x, int y){
+    cout<<x+y<<endl;}
+}
+namespace second{
+  void sum(float x, float y){
+    cout<<x+y<<endl;}
+}
+
+using namespace second;
+
+int main(){
+  sum(5,6);
+  sum(5.3,3.4);
+  sum(4.3,4);
+  sum(6,6.7);
+  //first::sum(5,4);
+  //second::sum(7.9,8.3);
+  return 0;}
+
+  
