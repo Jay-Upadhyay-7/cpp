49e11d7b78f8dace934007ccbde41d20edf8a8ab
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-09-30 10:16:39 +0530

    Create T1_Practical-05.cpp

---
+++ practical-05/T1_Practical-05.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+void charFunction(){
+  char a='A';
+  cout<<a<<endl;
+}
+void boolFunction(){
+  bool a;
+  cout<<a<<endl;
+}
+void shortFunction(){
+  short a;
+  cout<<a<<endl;
+}
+void intFunction(){
+  int a;
+  cout<<a<<endl;
+}
+void longFunction(){
+  long a;
+  cout<<a<<endl;
+}
+void floatFunction(){
+  float a;
+  cout<<a<<endl;
+}
+void doubleFunction(){
+  double a;
+  cout<<a<<endl;
+}
+void longdoubleFunction(){
+  long double a;
+  cout<<a<<endl;
+}
+void widecharFunction(){
+  wchar_t a;
+  cout<<a<<endl;
+}
+int main(){
+     charFunction();
+     boolFunction();
+     shortFunction();
+     intFunction();
+     longFunction();
+     floatFunction();
+     doubleFunction();
+     longdoubleFunction();
+     widecharFunction();
+     return 0;
+     }
