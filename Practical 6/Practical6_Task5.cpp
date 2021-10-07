6d4af4409425654f0ed1c1e885af7a328d2dfe08
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-10-07 11:58:15 -0400

    Update Practical6_Task5.cpp

--- Practical 6/Practical6_Task5.cpp
+++ Practical 6/Practical6_Task5.cpp
@@ -8,11 +8,14 @@ int main(){
      getline(cin,a);
      getline(cin,b);
      if(a.compare(b)==0){
-         cout<<"equal string "<<endl;}
+         cout<<"equal string "<<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;}
      else if(a.compare(b)>0){
-         cout<<"String a = "<< a <<"  is greater then b= "<< b <<endl;}
+         cout<<"String a = "<< a <<"  is greater then b= "<< b <<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;}
       else{
-         cout<<"string b = "<< b <<" is greater then a= "<< a <<endl;}
+         cout<<"string b = "<< b <<" is greater then a= "<< a <<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;}
 
      return 0;
 
