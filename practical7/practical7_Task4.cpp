0ae00c76253f304741fcfc009c728d7a483370de
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-10-13 08:37:19 -0400

    Update practical7_Task4.cpp

--- practical7/practical7_Task4.cpp
+++ practical7/practical7_Task4.cpp
@@ -9,15 +9,4 @@ for(int i=0;i<n;i++){
 
     cout<<endl;}
 
-return 0;}#include<iostream>
-using namespace std;
-int main(){
-int n=3,m=3;
-int a[n][m]={{1,2,3},{4,5,6},{7,8,9}};
-for(int i=0;i<n;i++){
-  for(int j=0;j<m;j++){
-    cout<<10-a[i][j]<<" ";}
-
-    cout<<endl;}
-
 return 0;}
