f7ecfd558f623b89e82b021bfe2397d92e3515f2
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-11-02 10:46:07 +0530

    Update task3.cpp

--- Practical 10/task3.cpp
+++ Practical 10/task3.cpp
@@ -1,3 +1,5 @@
+/*WAP to pass two variables in a parameterized constructor during object creation and have same names variables as class member data and constructor parameters. 
+Your job is to calculate the remainder of those two numbers.*/
 
 #include<iostream>
 using namespace std;
