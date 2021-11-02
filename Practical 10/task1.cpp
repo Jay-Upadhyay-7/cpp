53cb18942a94598244a95c3cd3ed69c15e0f6c52
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-11-02 11:18:14 +0530

    Update task1.cpp

--- Practical 10/task1.cpp
+++ Practical 10/task1.cpp
@@ -1,3 +1,10 @@
+/*WAP to create a directory that contains the following information.
+(a) Name of a person
+(b) Address
+(c) Telephone Number (if available with STD code)
+(d) Mobile Number (if available)
+(e) Head of the family*/
+
 #include<iostream>
 using namespace std;
 class student{
