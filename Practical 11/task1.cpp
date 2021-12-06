7b4c1db99f87246ac04024239f31757195016931
Jay-Upadhyay-7 <91174578+Jay-Upadhyay-7@users.noreply.github.com>
2021-12-06 10:02:14 +0530

    Create task1.cpp

---
+++ Practical 11/task1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class A{
+ public:
+     int x;
+     int y;
+     };
+  class B:public A{
+  public:
+  void display(){
+  x=10;
+  y=35;
+  cout<<x<<endl<<y<<endl;}
+  
+  };
+  int main(){
+  
+  B obj;
+  obj.x;
+  obj.y;
+  obj.display();}
